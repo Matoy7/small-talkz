cef2cff40a16a47c691594daddac01e8a284edfc
Matoy7 <yotam.eliraz@gmail.com>
2016-07-11 22:53:18 +0300

    s

---
+++ script.js
@@ -0,0 +1,85 @@
+ 
+var mymodule = angular.module('mymodule', ['ngRoute']);
+mymodule.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl : 'pages/enter.html',
+    controller  : 'SharedController'
+  }) 
+  .when('/chat', {
+    templateUrl : 'pages/chat.html',
+    controller  : 'cntrlChat'
+  });
+});
+
+
+mymodule.factory('myService', function() {
+ var savedData = {}
+ var set=function (data) {
+   savedData = data;
+ } 
+ function get() {
+  return savedData;
+}
+
+return {
+  set: set,
+  get: get
+}
+
+});
+
+mymodule.controller('SharedController', ['$scope', 'myService','$location',
+  function($scope, myService, $location){ 
+
+    $scope.updateMsg = function (data) {
+      myService.set(data);
+      $location.path("chat");
+    }
+
+    $scope.getMsg = function () {
+      $scope.message = myService.get();
+    }
+
+  }]);
+
+mymodule.controller("cntrlChat", ['$scope', 'myService','$q','$timeout',
+  function($scope, myService,$q,$timeout){ 
+   var socket = io();
+   $scope.messages = [];
+   $scope.room= myService.get().room;
+   $scope.name= myService.get().name;
+   socket.emit('room', $scope.room);
+
+   $scope.submit=function(){
+    socket.emit('chat_message',{ room: $scope.room, msg: $scope.name+": "+$scope.insertedText });
+    $scope.insertedText='';
+    return false; 
+  }
+
+  socket.on('chat_message', function(msg){
+    $scope.$apply(function() {
+      $scope.messages.push(msg);
+
+    });
+  });
+
+  socket.on('info_message', function(msg){
+    $scope.$apply(function() {
+      $scope.info=msg;
+
+    });
+  });
+
+
+  $scope.isUserTyping= function() {
+   var runTwoFunctionWithSleepBetweenThem=function(foo1, foo2, time) {
+     $q.when(foo1()).then(function() {
+       $timeout(foo2, time);
+     });
+   }
+   runTwoFunctionWithSleepBetweenThem(function (){socket.emit('info_message', { room: $scope.room, msg: $scope.name+' is typing...' })},
+    function (){socket.emit('info_message', { room: $scope.room, msg: '' });},1500); 
+ }
+}]);
+
---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/'));
+
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	io.emit('chat_message', "welcome");
+
+	socket.on('room', function(room) {
+		socket.join(room);
+	});
+
+	socket.on('chat_message', function(data){
+		io.sockets.in(data.room).emit('chat_message',data.msg);
+	});
+	socket.on('info_message', function(data){
+		socket.broadcast.to(data.room).emit('info_message',data.msg);
+	});
+
+	socket.on('disconnect', function(){
+		io.emit('chat message', "Bye");
+
+	});
+
+});
+
+http.listen(3000, function(){
+	console.log('listening on *:3000');
+});
