49d65e466fe04b457110349a274e41ec9c76599d
Matoy7 <yotam.eliraz@gmail.com>
2016-07-13 11:15:32 +0300

    s

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var app = express();
 var http = require('http').Server(app); 
 var io = require('socket.io')(http);
 
-app.use(express.static(process.cwd() + '/'));
+ 
+ 
+
 
 app.get('/', function(req, res){
-	res.sendFile('index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
