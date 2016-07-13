3cc4cae616d4ab0b847a92b443293958290668ae
Matoy7 <yotam.eliraz@gmail.com>
2016-07-13 13:17:07 +0300

    Update server.js
    s

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ io.on('connection', function(socket){
 });
 
 http.listen((process.env.PORT || 3000), function(){
-	console.log('listening on *:3000');
+	console.log('listening on *:3000 '+ __dirname);
 });
