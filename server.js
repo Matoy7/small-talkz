c77638e2525fb3c7dafe99ac7e53cc81f6230517
Matoy7 <yotam.eliraz@gmail.com>
2016-07-13 11:54:33 +0300

    Update server.js
    s

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app); 
 var io = require('socket.io')(http);
+var path = require('path');
 
- 
- 
 
+app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html');
