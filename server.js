cbe2c901a286792ee3c9b3a11849efc0184383f1
Yinon Eliraz <yinon@decor-d.com>
2016-07-16 22:30:12 +0300

    markup changes and css added

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var io = require('socket.io')(http);
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
+	res.sendFile('Index.html', { root: __dirname });
 });
 
 io.on('connection', function(socket){
