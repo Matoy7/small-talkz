c0c8b367a736103303ee510d82a3a5546cc6c324
elirazyo <yotame@hpe.com>
2016-07-20 15:40:48 +0300

    s

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var path = require('path');
 app.use(express.static(path.join(__dirname, '/')));
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/Index.html');
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
