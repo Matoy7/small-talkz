3de02cf8482acf8bd9a67eff6f380309df31b916
Matoy7 <yotam.eliraz@gmail.com>
2016-07-25 23:36:54 +0300

    dfgg

--- components/login/loginController.js
+++ components/login/loginController.js
@@ -1,3 +1,41 @@
+
+var express = require('express');
+var app = express();
+var http = require('http').Server(app); 
+var io = require('socket.io')(http);
+var path = require('path');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://Yotam:Yotam@ds023475.mlab.com:23475/small-talkz';
+MongoClient.connect(url, function(err, db) {
+	
+	assert.equal(null, err);
+
+	if (err) {
+		console.log(err);
+	}
+	else{
+		
+		console.log("Yes");
+
+	}
+	var cursor =db.collection('mydb').updateOne( { "name":"id"},
+		{$inc: { count: 1 } });
+	var x;
+	var cursor=db.collection('mydb').find( { "name":"id"} ) ;
+	cursor.each(function(err, doc) {
+		assert.equal(err, null);
+		if (doc != null) {
+			x=doc.count;
+			console.log(x);
+		}  
+	}); 
+	console.log(x);
+	console.log("Connected correctly to server.");
+	db.close();
+});
+
 smallTalkzModel.controller('loginController', ['$scope', 'sessionInfo','$location',
 	function($scope, sessionInfo, $location){ 
 
