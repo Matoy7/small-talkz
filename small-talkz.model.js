5f86fdbd14cf1c28d263441b4e3920715002d428
elirazyo <yotame@hpe.com>
2016-08-04 16:32:51 +0300

    sad

--- small-talkz.model.js
+++ small-talkz.model.js
@@ -2,31 +2,38 @@
 
 var smallTalkzModel = angular.module('smallTalkzModel', ['ngRoute']);
 smallTalkzModel.config(function($routeProvider) {
-  $routeProvider
-  .when('/', {
-    templateUrl : 'components/login/loginView.html',
-    controller  : 'loginController'
-  }) 
-  .when('/chat', {
-    templateUrl : 'components/chat/chatView.html',
-    controller  : 'chatController'
-  });
-});
+
+    $routeProvider
+    .when('/', {
+      templateUrl : 'components/login/loginView.html',
+      controller  : 'loginController'
+    }) 
+    .when('/chat', {
+      templateUrl : 'components/chat/chatView.html',
+      controller  : 'chatController'
+    });
+  }).run(function ($rootScope) {
+
+    // App is loading, so set isAppLoading to true and start a timer
+    console.log($rootScope);
+    $rootScope.isAppLoading = true;
+
+  });;
 
 
-smallTalkzModel.factory('sessionInfo', function() {
- var savedInfo = {}
- var set=function (info) {
-   savedInfo = info;
- } 
- function get() {
-  return savedInfo;
-}
+  smallTalkzModel.factory('sessionInfo', function() {
+   var savedInfo = {}
+   var set=function (info) {
+     savedInfo = info;
+   } 
+   function get() {
+    return savedInfo;
+  }
 
-return {
-  set: set,
-  get: get
-}
+  return {
+    set: set,
+    get: get
+  }
 
 });
 
