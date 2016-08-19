27199d71286f131c9f536b44e486ef38580bffc4
Yinon Eliraz <yinon@decor-d.com>
2016-08-19 15:38:45 +0300

    css changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,7 @@
 $(document).ready(function() {
-
-	if($("#loginPage").length > 0){
- 
-	  	$("#owl-example").owlCarousel({
-			items : 1, //10 items above 1000px browser width
-			itemsDesktop : [1000,1], //5 items between 1000px and 901px
-			itemsDesktopSmall : [900,1], // betweem 900px and 601px
-			itemsTablet: [600,1], //2 items between 600 and 0
-			itemsMobile : false, // itemsMobile disabled - inherit from itemsTablet option
-			pagination: false,
-			animateOut: 'slideOutDown',
-	    	animateIn: 'flipInX',
-	    	touchDrag  : false,
-     		mouseDrag  : false
-
+	if($(".chat").length > 0){
+		$(".toggle-users").on("click",function(){
+			$(".users").toggleClass("visible");
 		});
-		$("#owl-example").data('owlCarousel').reinit({
-		     touchDrag  : false,
-		     mouseDrag  : false
-		 });
-		var owl = $("#owl-example").data('owlCarousel');
-		$("#join").on("click", function(e){
-			owl.goTo(1);
-		});
-		$("#newConvers").on("click", function(e){
-			owl.goTo(0);
-		});
-
 	}
-
 });
