abc5c94d39b753d3f0bbb8fd69adddb44a1fdb0f
Matoy7 <yotam.eliraz@gmail.com>
2016-09-20 21:26:33 +0300

    ssad

--- server.js
+++ server.js
@@ -1,29 +1,27 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app); 
-var path = require('path');
-var assert = require('assert');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
 
+ // ---------------- define third Party dependencies)
+ var express = require('express');
+ var app = express();
+ var http = require('http').Server(app); 
+ var path = require('path');
+ var assert = require('assert');
+ var mongoose = require('mongoose');
+ var bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({
-	extended: true
-}));
+ // ---------------- config the app
+ app.use(bodyParser.urlencoded({
+ 	extended: true
+ }));
+ app.use(bodyParser.json());
+ app.use(express.static(path.join(__dirname, '/')));
 
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, '/')));
+ // ---------------- define the server handlers (internal dependencies)
+ var dbHandler = require('./server_modules/dbHandler.js');
+ var httpRequesetHandler = require('./server_modules/httpRequesetHandler.js').getHttpRequestHandler(dbHandler, app);
+ var io = require('./server_modules/socketHandler.js').listen(http, dbHandler);
 
-var dbHandler = require('./server_handlers/dbHandler.js');
-var httpRequesetHandler = require('./server_handlers/httpRequesetHandler.js').getHttpRequestHandler(dbHandler, app);
-
- 
-
-var io = require('./server_handlers/socketHandler.js').listen(http, dbHandler);
-
-
-    http.listen((process.env.PORT || 3000), function(){
-    	 dbHandler.get_random_room();
-
-    	console.log('listening on *:3000 '+ __dirname);
-    });
+ // ---------------- start the server
+ http.listen((process.env.PORT || 3000), function(){
+ 	dbHandler.get_random_room();
+ 	console.log('listening on *:3000 '+ __dirname);
+ });
